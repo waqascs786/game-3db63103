c99269196438d6136b475adfb18c7e45f2491c5c
waqascs786 <waqas.cs786@gmail.com>
2026-08-26 13:03:46 +0500

    Initial game source code

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/foundation.dart' show defaultTargetPlatform, TargetPlatform;
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'screens/splash_screen.dart';
@@ -6,12 +7,17 @@ import 'theme/app_theme.dart';
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
-    options: const FirebaseOptions(
+    options: FirebaseOptions(
       apiKey: 'AIzaSyAzN4vJZlYmGVzm8sCsz1bEPawQEiKIc6k',
-      appId: '1:311998863107:android:2853688e5735168c2cfb36',
+      appId: defaultTargetPlatform == TargetPlatform.iOS
+          ? '1:311998863107:ios:placeholder'
+          : defaultTargetPlatform == TargetPlatform.android
+              ? '1:311998863107:android:placeholder'
+              : '1:311998863107:web:76d4b682624e96d02cfb36',
       messagingSenderId: '311998863107',
       projectId: 'trivianinja-bff5c',
       storageBucket: 'trivianinja-bff5c.firebasestorage.app',
+      iosBundleId: defaultTargetPlatform == TargetPlatform.iOS ? 'com.trivianinja.app' : null,
     ),
   );
   runApp(const TriviaGameApp());
